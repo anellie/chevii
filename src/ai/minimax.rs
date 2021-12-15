edaffbf60b4993812567a9444482a638ff4540f9
Ellie Ang <git@angm.xyz>
2021-12-15 04:00:29 +0100

    Remove unneeded bookkeeping code

--- src/ai/minimax.rs
+++ src/ai/minimax.rs
@@ -9,16 +9,8 @@ use crate::ai::table::{TransTable, Entry};
 const INF: isize = 999999999999;
 const WIN: isize = 99999999;
 
-struct Suggestion {
-    play: RatedMove,
-    expected_move: Option<ChessMove>
-}
-
 pub fn calculate_move(board: Board, time: f32) -> ChessMove {
-    let mut suggest = Suggestion {
-        play: (Default::default(), 0),
-        expected_move: None
-    };
+    let mut play = (Default::default(), 0);
     let table = TransTable::new();
     let mut moves = evaluation::sorted_moves(&board);
 
@@ -41,48 +33,30 @@ pub fn calculate_move(board: Board, time: f32) -> ChessMove {
     while start_time.elapsed().as_secs_f32() < time {
         if let Ok(value) = suggest_rx.try_recv() {
             stop_tx.send(false).ok();
-            suggest = value;
+            play = value;
         }
         thread::sleep(Duration::from_millis(1));
     }
     stop_tx.send(true).ok();
     drop(stop_tx);
 
-    if let Some(expected) = suggest.expected_move {
-        log::info!(
-            "Playing {} (score {}), expecting {}",
-            suggest.play.0,
-            suggest.play.1,
-            expected
-        );
-    } else {
-        log::info!(
-            "Playing {} (score {}, checkmate)",
-            suggest.play.0,
-            suggest.play.1,
-        );
-    }
-
-    suggest.play.0
+    play.0
 }
 
-fn calc_depth(board: Board, table: &TransTable, depth: isize, mut moves: Vec<RatedMove>) -> (Suggestion, Vec<RatedMove>) {
+fn calc_depth(board: Board, table: &TransTable, depth: isize, mut moves: Vec<RatedMove>) -> (RatedMove, Vec<RatedMove>) {
     let res = moves
         .par_iter_mut()
         .map(|(mov, score)| {
             let clone = board.make_move_new(*mov);
-            let (basic_score, expected) = minimax(&clone, table, depth - 1, board.side_to_move(), -INF, INF);
+            let basic_score = minimax(&clone, table, depth - 1, board.side_to_move(), -INF, INF);
             *score = basic_score + evaluation::eval_move(&board, *mov);
-            ((*score, expected), *mov)
+            (*score, *mov)
         })
-        .max_by_key(|(score, _)| score.0)
+        .max_by_key(|(score, _)| *score)
         .unwrap();
 
     moves.par_sort_unstable_by_key(|mov| -mov.1);
-    (Suggestion {
-        play: (res.1, (res.0).0),
-        expected_move: (res.0).1
-    }, moves)
+    ((res.1, res.0), moves)
 }
 
 fn minimax(
@@ -92,24 +66,24 @@ fn minimax(
     player: Color,
     mut alpha: isize,
     mut beta: isize,
-) -> (isize, Option<ChessMove>) {
+) -> isize {
     let hash = board.get_hash();
     match table.get(hash) {
-        Some(entry) if entry.depth >= depth as i32 => return (entry.score as isize, None),
+        Some(entry) if entry.depth >= depth as i32 => return entry.score as isize,
         _ => (),
     }
 
     let moves = match board.status() {
-        BoardStatus::Checkmate if board.side_to_move() == player => return (-(WIN + (depth * 1000) as isize), None),
-        BoardStatus::Checkmate => return (WIN + (WIN * depth as isize), None),
-        BoardStatus::Stalemate => return (-WIN / 2, None),
+        BoardStatus::Checkmate if board.side_to_move() == player => return -(WIN + (depth * 1000) as isize),
+        BoardStatus::Checkmate => return WIN + (WIN * depth as isize),
+        BoardStatus::Stalemate => return -WIN / 2,
 
         BoardStatus::Ongoing if depth <= 0 => {
             let capture_moves = evaluation::capturing_moves(board);
             if !capture_moves.is_empty() {
                 capture_moves
             } else {
-                return (evaluation::eval_board(board, player), None);
+                return evaluation::eval_board(board, player);
             }
         },
 
@@ -117,13 +91,12 @@ fn minimax(
     };
 
     let mut tmp = board.clone();
-    let (score, mov) = if board.side_to_move() == player {
+    let score = if board.side_to_move() == player {
         let mut max_score = -INF;
-        let mut best_move = ChessMove::default();
 
         for (mov, _) in moves {
             board.make_move(mov, &mut tmp);
-            let (score, _) = minimax(&tmp, table, depth - 1, player, alpha, beta);
+            let score = minimax(&tmp, table, depth - 1, player, alpha, beta);
 
             max_score = isize::max(max_score, score);
             alpha = isize::max(alpha, max_score);
@@ -133,18 +106,16 @@ fn minimax(
 
             if score >= max_score {
                 max_score = score;
-                best_move = mov;
             }
         }
 
-        (max_score, Some(best_move))
+        max_score
     } else {
         let mut min_score = INF;
-        let mut best_move = ChessMove::default();
 
         for (mov, _) in moves {
             board.make_move(mov, &mut tmp);
-            let (score, _) = minimax(&tmp, table, depth - 1, player, alpha, beta);
+            let score = minimax(&tmp, table, depth - 1, player, alpha, beta);
 
             min_score = isize::min(min_score, score);
             beta = isize::min(beta, min_score);
@@ -154,11 +125,10 @@ fn minimax(
 
             if score <= min_score {
                 min_score = score;
-                best_move = mov;
             }
         }
 
-        (min_score, Some(best_move))
+        min_score
     };
 
     table.put(Entry {
@@ -166,5 +136,5 @@ fn minimax(
         score: score as i32,
         depth: depth as i32
     });
-    (score, mov)
+    score
 }
